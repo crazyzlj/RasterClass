e310c2d5a5346596717b37e6ffa0da9126ee2f40
crazyzlj <crazyzlj@gmail.com>
2017-03-17 22:20:38 +0800

    debug travis ci

--- ci/travis/linux_gcc48/before_install.sh
+++ ci/travis/linux_gcc48/before_install.sh
@@ -9,15 +9,15 @@ export CXX="g++-4.8" CC="gcc-4.8"
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 # download gdal from osgeo, and compile and install
 GDAL_VERSION=1.11.5
-wget -q http://download.osgeo.org/gdal/$GDAL_VERSION/gdal-$GDAL_VERSION.tar.gz
-tar xvzf gdal-$GDAL_VERSION.tar.gz
-cd gdal-$GDAL_VERSION
-./configure
-make -j 4
-sudo make install
+#wget -q http://download.osgeo.org/gdal/$GDAL_VERSION/gdal-$GDAL_VERSION.tar.gz
+#tar xvzf gdal-$GDAL_VERSION.tar.gz
+#cd gdal-$GDAL_VERSION
+#./configure
+#make -j 4
+#sudo make install
 # download mongo-c-driver from github, and compile and install
 MONGOC_VERSION=1.6.1
-wget -q https://github.com/mongodb/mongo-c-driver/archive/$MONGOC_VERSION.tar.gz
+wget -q https://github.com/mongodb/mongo-c-driver/releases/download/$MONGOC_VERSION/mongo-c-driver-$MONGOC_VERSION.tar.gz
 tar xzf mongo-c-driver-$MONGOC_VERSION.tar.gz
 cd mongo-c-driver-$MONGOC_VERSION
 ./configure --disable-automatic-init-and-cleanup
