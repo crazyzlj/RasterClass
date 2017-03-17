934c648e4892e7db860e54ab13dfff97b581153d
crazyzlj <crazyzlj@gmail.com>
2017-03-17 20:40:28 +0800

    debug travis ci

--- ci/travis/linux_gcc48/before_install.sh
+++ ci/travis/linux_gcc48/before_install.sh
@@ -2,7 +2,11 @@
 
 set -e
 # update gcc to version 4.8
-sudo apt-get install gcc-4.8 g++-4.8
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+sudo apt-get install -qq gcc-4.8 g++-4.8
+export CXX="g++-4.8" CC="gcc-4.8"
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 # download gdal from osgeo, and compile and install
 GDAL_VERSION=1.11.5
 wget -q http://download.osgeo.org/gdal/gdal-$GDAL_VERSION.tar.gz
