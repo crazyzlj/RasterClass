623ce1df71821d08323563b6f48696877f952392
crazyzlj <crazyzlj@gmail.com>
2017-03-18 12:49:26 +0800

    update ci shell

--- ci/travis/linux_gcc48/before_install.sh
+++ ci/travis/linux_gcc48/before_install.sh
@@ -17,7 +17,7 @@ GDAL_VERSION=1.11.5
 #make -j4
 #sudo make install
 # install gdal from apt-get
-sudo apt-get install -qq gdal-bin libgdal1-dev libgdal1
+sudo apt-get install -qq gdal-bin libgdal-dev libgdal
 # download mongo-c-driver from github, and compile and install
 cd ..
 MONGOC_VERSION=1.6.1
