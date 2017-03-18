635ae69d892f748e2b883e808f96e256d381cf02
crazyzlj <crazyzlj@gmail.com>
2017-03-18 12:26:43 +0800

    update ci shell

--- ci/travis/linux_gcc48/before_install.sh
+++ ci/travis/linux_gcc48/before_install.sh
@@ -8,16 +8,16 @@ sudo apt-get install -qq gcc-4.8 g++-4.8
 export CXX="g++-4.8" CC="gcc-4.8"
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 # download gdal from osgeo, and compile and install
-GDAL_VERSION=1.11.5
-cd ..
-wget -q http://download.osgeo.org/gdal/$GDAL_VERSION/gdal-$GDAL_VERSION.tar.gz
-tar xvzf gdal-$GDAL_VERSION.tar.gz
-cd gdal-$GDAL_VERSION
-./configure
-make -j4
-sudo make install
-cd ..
+#GDAL_VERSION=1.11.5
+#cd ..
+#wget -q http://download.osgeo.org/gdal/$GDAL_VERSION/gdal-$GDAL_VERSION.tar.gz
+#tar xvzf gdal-$GDAL_VERSION.tar.gz
+#cd gdal-$GDAL_VERSION
+#./configure
+#make -j4
+#sudo make install
 # download mongo-c-driver from github, and compile and install
+cd ..
 MONGOC_VERSION=1.6.1
 wget -q https://github.com/mongodb/mongo-c-driver/releases/download/$MONGOC_VERSION/mongo-c-driver-$MONGOC_VERSION.tar.gz
 tar xzf mongo-c-driver-$MONGOC_VERSION.tar.gz
